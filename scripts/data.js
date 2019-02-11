ecb529790c1ef81403ee9df8e81961e41828a743
doniyor2109 <doniyor2109@gmail.com>
2019-02-11 18:28:27 +0500

    Restructure project

--- scripts/data.js
+++ scripts/data.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const loadLogos = require('shields.io/lib/load-logos');
 const loadSimpleIcons = require('shields.io/lib/load-simple-icons');
 const { collectDefinitions } = require('shields.io/core/base-service/loader')
+const servicesList = require('../lib/services');
 
 const { services, categories } = collectDefinitions();
 
@@ -11,54 +12,16 @@ const logos = {
   ...loadSimpleIcons(),
 };
 
-const supportedParams = [
-  'NodeVersion_packageName',
-  'NpmCollaborators_packageName',
-  'NpmDownloads_packageName',
-  'Jitpack_artifactId',
-  'Jitpack_groupId',
-  'MavenCentral_groupId',
-  'MavenCentral_artifactId',
-  'Coveralls_vcsType',
-  'Codecov_vcsName',
-  'CircleCi_vcsType',
-  'CircleCi_owner',
-  'CircleCi_repo',
-  'CircleCi_branch',
-];
+const mockConfigs = {
+  github: {},
+  npm: {},
+  maven: {},
+  bower: {},
+};
 
-const defaultValues = supportedParams.reduce((current, key) => {
-  current[key] = '__UNDEFINED__';
-  return current;
-}, {});
-
-const flattenedServices = services
-  .reduce((accum, current, outer) => {
-    const { examples, name } = current;
-
-    const logo = Object.keys(logos).find(logo => name.toLowerCase().includes(logo));
-
-    return accum.concat(
-      examples.map((item, inner) => {
-        const { example, queryParams } = item;
-        const namedParams = {};
-
-        for (const param in example.namedParams) {
-          example.pattern = example.pattern.replace(param, `${name}_${param}`);
-          namedParams[`${name}_${param}`] = '_UNDEFINED_';
-        }
-        example.namedParams = namedParams;
-        example.queryParams = { ...queryParams, logo };
-
-        return Object.assign(item, {id: `${outer}-${inner}`, name });
-      })
-    );
-  }, [])
-  .filter(({ example }) => {
-    const tokens = Object.keys(example.namedParams);
-    return tokens.every(i => supportedParams.includes(i));
-  });
-
+function findLogo(name) {
+  return Object.keys(logos).find(logo => name.toLowerCase().includes(logo));
+}
 
 function writeJSON(filename, data) {
   fs.writeFileSync(
@@ -67,7 +30,42 @@ function writeJSON(filename, data) {
   );
 }
 
-writeJSON('services.json', flattenedServices);
+const flattenedServices = services
+  .reduce((accum, current, outer) => {
+    const { examples, name } = current;
+    const logo = findLogo(name);
+
+    return accum.concat(
+      examples.map((item, inner) => {
+        const { example, queryParams } = item;
+
+        example.queryParams = { ...queryParams, logo };
+
+        return Object.assign(item, {id: `${outer}-${inner}`, name });
+      })
+    );
+  }, [])
+  .filter(({ example, name }) => {
+    const tokens = Object.keys(example.namedParams);
+    const service = servicesList[name];
+    return service && tokens.every(key => service.getParams(mockConfigs).hasOwnProperty(key));
+  });
+
+const defaultValues = Object.keys(servicesList).reduce((curr, key) => {
+  const params = servicesList[key].getParams(mockConfigs);
+
+  for (const param in params) {
+    params[param] = 'unknown';
+  }
+  return {
+    ...curr,
+    ...params,
+  };
+}, {});
+
+
+
+writeJSON('services_list.json', flattenedServices);
 writeJSON('default_value.json', defaultValues);
 writeJSON('categories.json', categories);
 
